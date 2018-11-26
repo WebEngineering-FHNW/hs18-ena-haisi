d672eb66594bc46f4219d895c94075dc95414bb5
Hasan Kara <hasan.kara@students.fhnw.ch>
2018-11-26 10:24:08 +0100

    Ignore BootStrap in Test-Environment

--- grails-app/init/webec/BootStrap.groovy
+++ grails-app/init/webec/BootStrap.groovy
@@ -1,5 +1,7 @@
 package webec
 
+import grails.util.Environment
+
 import java.nio.file.Files
 
 class BootStrap {
@@ -7,6 +9,10 @@ class BootStrap {
     def grailsApplication
 
     def init = { servletContext ->
+
+        if (Environment.current == Environment.TEST) {
+            return
+        }
 
         def brockmann = new Artist(name: "Josef Müller-Brockmann", description: "Josef Müller-Brockmann war ein Schweizer Grafikdesigner, Typograf, Autor und Lehrer. Müller-Brockmann war führender Theoretiker und Praktiker der Schweizer Typografie.").save()
         def hamburg = new Artist(name: "Jörg Hamburg", description: "Der Zürcher Grafiker, Jahrgang 1935, schliesst seine Ausbildung in der Fachklasse der Allgemeinen Gewerbeschule in Basel bei Armin Hofmann, Emil Ruder und Donald Brun 1954 ab. Anschliessend arbeitet er bis 1958 im Werbeatelier der J.R. Geigy AG, Basel, später im Werbeatelier Josef Müller-Brockmann in Zürich (1958–1961). Ihm assistiert er ab 1958 als Leiter der Fachklasse für Grafik an der KGSZ. Seine dortige Lehrtätigkeit beginnt er mit Schriftunterricht im Vorkurs und später als Dozent für Fotografik, Typografie und Layout an der Fachklasse für Fotografie. 1961 gründet er zusammen mit Alfred Aebersold und Herbert Merz die Gruppe 3, ein Atelier für Gestaltung. Zu den herausragenden Arbeiten von Jörg Hamburger gehören die Gestaltung und Bewerbung der Ausstellung «Modellfall Citroën» im KGMZ und zahlreiche vom EDI ausgezeichnete Plakate für Ausstellungen und den Jugendsport").save()
